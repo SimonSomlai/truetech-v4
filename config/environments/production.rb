a068382b675986110b9267e26478bbed2045f80b
SimonSomlai <simon_somlai@hotmail.com>
2016-10-21 17:12:12 +0200

    better'

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,7 +92,7 @@ Rails.application.configure do
     :metastore    => client,
     :entitystore  => client
   }
-  config.static_cache_control = "public, max-age=2592000"
+  config.public_file_server.headers = { 'Cache-Control' => 'public, max-age=2592000' }
 
   config.action_dispatch.default_headers = {
     'X-Frame-Options' => 'ALLOWALL'
