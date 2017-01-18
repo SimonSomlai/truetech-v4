a669ea9956fd23a905eca892ec1cd734d48b4446
SimonSomlai <simon_somlai@hotmail.com>
2017-01-18 15:58:03 +0100

    Recompiled assets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -26,20 +26,6 @@ trumbowyg.min.css
 highlight.min.css )
 
 # JS files
-<<<<<<< HEAD
-Rails.application.config.assets.precompile += %w( trumbowyg.min.js )
-Rails.application.config.assets.precompile += %w( jquery.tagsinput.js )
-Rails.application.config.assets.precompile += %w( rangeslider.min.js )
-Rails.application.config.assets.precompile += %w( functions.js )
-Rails.application.config.assets.precompile += %w( plugins.js )
-Rails.application.config.assets.precompile += %w( Chart.min.js )
-Rails.application.config.assets.precompile += %w( custom.js )
-Rails.application.config.assets.precompile += %w( jquery.js )
-Rails.application.config.assets.precompile += %w( jquery_ujs.js )
-Rails.application.config.assets.precompile += %w( jquery.remotipart.js )
-Rails.application.config.assets.precompile += %w( script.js )
-Rails.application.config.assets.precompile += %w( ahoy.js )
-=======
 Rails.application.config.assets.precompile += %w( trumbowyg.min.js
 jquery.tagsinput.js
 rangeslider.min.js
@@ -54,4 +40,3 @@ script.js
 ahoy.js
 highlight.min.js
  )
->>>>>>> technical_articles
