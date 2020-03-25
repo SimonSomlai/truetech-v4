d0bb4a3151da60dd485c22291bbec2715ffb996a
Simon Somlai <simon@Simon.local>
2020-03-25 11:33:13 +0100

    Redirect all traffic to apex domain with a translation path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,16 +5,26 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   protect_from_forgery with: :null_session, if: Proc.new { |c| c.request.format == 'application/json' }
 
-  before_action :set_locale
+  before_action :set_locale_and_redirect
 
   private
 
-  def set_locale
+  # Redirect all traffic to apex domain with a translation path
+  def redirect_to_path(path)
+    redirect_to "https://simonsomlai.com#{path.chomp("/")}", :status => :moved_permanently 
+  end
+
+  def is_old?
+    request.host == 'truetech.be' || request.subdomain == "www"
+  end
+
+  def set_locale_and_redirect
     if params[:locale].present?
       I18n.locale = params[:locale].split("?")[0]
+      redirect_to_path(request.path) if(is_old?) 
     else
       I18n.locale = I18n.default_locale
-      redirect_to "/#{I18n.locale}#{request.path}"
+      redirect_to_path "/#{I18n.locale}#{request.path}"
     end
   end
 
